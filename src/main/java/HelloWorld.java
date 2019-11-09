2007593a7aa7d1c179aff4215b7de0b3dbb6deae
Sander van den Oever <S.Y.vandenOever@tudelft.nl>
2019-11-10 00:04:16 +0100

    Initial project setup

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Student student = new Student("Iron Man", 2345678);
+
+        System.out.println("Hi there, " + student);
+    }
+}
