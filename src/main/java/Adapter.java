2f035bb2147ebf154a505898232052f4300e35cc
Shaan <s.hossain@student.tudelft.nl>
2019-11-21 16:41:26 +0100

    Attempt adding jdbc

---
+++ src/main/java/Adapter.java
@@ -0,0 +1,63 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Adapter {
+    /* 01 Database variables ------------------------------- */
+    static Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    /* 02 Variables ---------------------------------------- */
+    String jdbcUrl = "jdbc:postgresql://ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/dains1dt33rtkd";
+
+    String username = "lfxghibojyjdle";
+    String password = "0dc939d4eb5bd22284f2fe0aed23351b366c098a2bf6cf42f9fc697c0d6ba6d7";
+
+    /* 03 Constructor for DbAdapter ------------------------ */
+    public Adapter() {
+    }
+
+    /**
+     *  Connect to the database.
+     */
+    public void connect() {
+        try {
+            // Step 2 - Open connection
+            conn = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // Print connected
+            System.out.println("DbAdapter: Connection to database established");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } // connect
+
+    /**
+     * Disconnect from database.
+     */
+    public void disconnect() {
+        try {
+
+            // Step 5 Close connection
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (rs != null) {
+                rs.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+            // Print connected
+            System.out.println("DbAdapter: Connection to database closed");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } // disconnect
+}
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,12 @@ public class HelloWorld {
         Student student = new Student("Iron Man", 2345678);
 
         System.out.println("Hi there, " + student);
+
+        String[] queries = new String[1];
+        int id = 0;
+        String username = "test";
+        String password = "pass";
+        queries[0] = "INSERT INTO points VALUES ('" + id + "', '" + username + "', '" + password + "'+";
+        Query.runQueries(queries);
     }
 }
