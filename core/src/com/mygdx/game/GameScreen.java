3d305ab6a0544a7d1c2a25adfd850956ce98aa51
bprovanbessell <B.L.S.Provan-Bessell@student.tudelft.nl>
2019-11-25 16:38:16 +0100

    Add implementation for the Paddles. They now appear on the board.

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -13,8 +14,13 @@ public class GameScreen implements Screen {
     //initially we need a texture for the paddle
     Texture puckImage;
 
+    Texture paddle1Image;
+    Texture paddle2Image;
+
     //we can use the circle class for the puck
     Puck puck;
+
+    Paddle paddle1, paddle2;
 
     OrthographicCamera camera;
 
@@ -25,12 +31,20 @@ public class GameScreen implements Screen {
 
         puckImage = new Texture(Gdx.files.internal("hockey-puck.png"));
 
+        paddle1Image = new Texture(Gdx.files.internal("bluePaddle.png"));
+        paddle2Image = new Texture(Gdx.files.internal("redPaddle.png"));
+
         camera = new OrthographicCamera();
         //we can change the resolution to whatever is appropriate later
         camera.setToOrtho(false, 400, 300);
 
         //we should later change it to the res0lution and so on...
         puck = new Puck(200f, 150f, 0f, 30f, 15f);
+
+        //add the pucks
+        paddle1 = new Paddle(100f, 150f, 0f, 0f, 20f);
+
+        paddle2 = new Paddle(300f, 150f, 0f, 0f, 25f);
 
     }
 
@@ -57,12 +71,22 @@ public class GameScreen implements Screen {
         //draw the puck as the texture and in the place that the puck exists
         //Maybe there is some border, or the radius doesn't perfectly scale up the image
         //the boundary is still not totally correct
-        game.spriteBatch.draw(puckImage, puck.x - puck.radius, puck.y-puck.radius, puck.radius*2, puck.radius*2);
+        game.spriteBatch.draw(puckImage, puck.x - puck.radius, puck.y-puck.radius,
+                puck.radius*2, puck.radius*2);
+
+        game.spriteBatch.draw(paddle1Image, paddle1.x - paddle1.radius, paddle1.y - paddle1.radius,
+                paddle1.radius*2, paddle1.radius*2);
+
+        game.spriteBatch.draw(paddle2Image, paddle2.x - paddle2.radius, paddle2.y - paddle2.radius,
+                paddle2.radius*2, paddle2.radius*2);
 
         game.spriteBatch.end();
 
         //move the puck
-        puck.movePuck(Gdx.graphics.getDeltaTime());
+
+        float deltaTime = Gdx.graphics.getDeltaTime();
+
+        puck.movePuck(deltaTime);
         //ensure it is within boundaries
         puck.FixPosition();
 
---
+++ core/src/com/mygdx/game/Paddle.java
@@ -0,0 +1,82 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Circle;
+
+public class Paddle extends Circle {
+
+    public float xSpeed, ySpeed;
+
+    /**
+     * Constructor
+     * @param x
+     * @param y
+     * @param xSpeed
+     * @param ySpeed
+     * @param radius
+     */
+    public Paddle(float x, float y, float xSpeed, float ySpeed, float radius) {
+
+        super(x, y, radius);
+
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    /**
+     * Method to move the paddle, this can be expanded later to be a more complex and realistic
+     * @param rightPressed
+     * @param leftPressed
+     * @param upPressed
+     * @param downPressed
+     * @param deltaTime
+     */
+    public void movePaddle(boolean rightPressed, boolean leftPressed,
+                           boolean upPressed, boolean downPressed,
+                           float deltaTime) {
+        if (rightPressed) {
+            this.x += 30*deltaTime;
+        }
+
+        if (leftPressed) {
+            this.x -= 30*deltaTime;
+        }
+
+        if (upPressed) {
+            this.x += 30*deltaTime;
+        }
+
+        if (downPressed) {
+            this.x -= 30*deltaTime;
+        }
+
+    }
+
+    /**
+     * Method to ensure the puck is within the correct boundaries
+     */
+    public void FixPosition() {
+        //we need to add the functionality to check that if the puck has hit the boundaries
+        //or has been hit by a paddle
+
+        //boundary detection
+        //the puck.x and y represent the center of the circle
+
+        //change later to reverse the speed and so on, but we need a co-efficient of friction
+        //and more physics stuff
+        if (this.x - this.radius < 0) {
+            this.x = 0 + this.radius;
+        }
+        if (this.x > 400 - this.radius) {
+            this.x = 400 - this.radius;
+        }
+
+        if (this.y - this.radius < 0) {
+            this.y = 0 + this.radius;
+        }
+        if (this.y > 300 - this.radius) {
+            this.y = 300 - this.radius;
+        }
+    }
+
+
+}
