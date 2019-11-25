bd3b8ab795595797aca4eeddf0c9ebf0d43e27c7
Shaan <s.hossain@student.tudelft.nl>
2019-11-25 17:49:43 +0100

    Auto stash before rebase of "refs/heads/master"

---
+++ core/test/test.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class test {
+    @Test
+    public void test1() {
+        assertEquals(0,1);
+    }
+}
