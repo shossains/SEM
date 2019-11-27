d594df1c745fb41773b19e9402216e8d544586bd
Krzysztof Garbowicz <k.m.garbowicz@student.tudelft.nl>
2019-11-27 21:09:10 +0100

    Board Implementation on new branch

---
+++ src/main/java/Board.java
@@ -0,0 +1,15 @@
+import com.badlogic.gdx.math.Rectangle;
+
+public class Board extends Rectangle {
+
+    /**
+     * The board object.
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param width Boards width
+     * @param height Boards height
+     */
+    public Board(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+}
